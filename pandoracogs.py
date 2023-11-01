b57ad5538f9c81a42b54e09ca2fadd93d4adcc7f
Archael <Archael@hotmail.ca>
2023-11-01 04:29:12 -0400

    3.4 added asyncio locks on cogs hopefully helps mitigate interaction issues during update periods

--- pandoracogs.py
+++ pandoracogs.py
@@ -10,6 +10,7 @@ import menus
 class StaminaCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.lock = asyncio.Lock()
         self.stamina_manager.start()
 
     def cog_unload(self):
@@ -17,14 +18,15 @@ class StaminaCog(commands.Cog):
 
     @tasks.loop(seconds=600)
     async def stamina_manager(self):
-        print("Stamina Assignment Triggered!")
-        player_list = player.get_all_users()
-        if player_list:
-            for player_user in player_list:
-                new_stamina_value = player_user.player_stamina + 10
-                if new_stamina_value > 5000:
-                    new_stamina_value = 5000
-                player_user.set_player_field("player_stamina", new_stamina_value)
+        async with self.lock:
+            print("Stamina Assignment Triggered!")
+            player_list = player.get_all_users()
+            if player_list:
+                for player_user in player_list:
+                    new_stamina_value = player_user.player_stamina + 10
+                    if new_stamina_value > 5000:
+                        new_stamina_value = 5000
+                    player_user.set_player_field("player_stamina", new_stamina_value)
 
 
 class RaidCog(commands.Cog):
@@ -35,6 +37,7 @@ class RaidCog(commands.Cog):
         self.channel_num = channel_num
         self.sent_message = sent_message
         self.channel_object = channel_object
+        self.lock = asyncio.Lock()
         self.raid_manager.start()
         print(f"Channel #{self.channel_num}: RaidCog Running")
 
@@ -43,17 +46,18 @@ class RaidCog(commands.Cog):
 
     @tasks.loop(seconds=60)
     async def raid_manager(self):
-        is_alive = await self.bot.raid_boss(self.active_boss, self.channel_id, self.channel_num,
-                                            self.sent_message, self.channel_object)
-        if not is_alive:
-            bosses.clear_boss_info(self.channel_id, 0)
-            level, boss_type, boss_tier = bosses.get_boss_details(self.channel_num)
-            active_boss = bosses.spawn_boss(self.channel_id, 0, boss_tier, boss_type, level, self.channel_num)
-            self.active_boss = active_boss
-            embed_msg = active_boss.create_boss_msg(0, True)
-            raid_button = menus.RaidView()
-            sent_message = await self.channel_object.send(embed=embed_msg, view=raid_button)
-            self.sent_message = sent_message
+        async with self.lock:
+            is_alive = await self.bot.raid_boss(self.active_boss, self.channel_id, self.channel_num,
+                                                self.sent_message, self.channel_object)
+            if not is_alive:
+                bosses.clear_boss_info(self.channel_id, 0)
+                level, boss_type, boss_tier = bosses.get_boss_details(self.channel_num)
+                active_boss = bosses.spawn_boss(self.channel_id, 0, boss_tier, boss_type, level, self.channel_num)
+                self.active_boss = active_boss
+                embed_msg = active_boss.create_boss_msg(0, True)
+                raid_button = menus.RaidView()
+                sent_message = await self.channel_object.send(embed=embed_msg, view=raid_button)
+                self.sent_message = sent_message
 
 
 class SoloCog(commands.Cog):
@@ -64,6 +68,7 @@ class SoloCog(commands.Cog):
         self.channel_id = channel_id
         self.sent_message = sent_message
         self.channel_object = channel_object
+        self.lock = asyncio.Lock()
         print(f"{self.player_object.player_username}: SoloCog Running")
 
     async def run(self):
@@ -74,7 +79,8 @@ class SoloCog(commands.Cog):
 
     @tasks.loop(seconds=60)
     async def solo_manager(self):
-        is_alive = await self.bot.solo_boss(self.player_object, self.active_boss,
-                                            self.channel_id, self.sent_message, self.channel_object)
-        if not is_alive:
-            self.cog_unload()
+        async with self.lock:
+            is_alive = await self.bot.solo_boss(self.player_object, self.active_boss,
+                                                self.channel_id, self.sent_message, self.channel_object)
+            if not is_alive:
+                self.cog_unload()
